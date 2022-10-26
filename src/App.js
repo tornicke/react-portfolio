e177f0b863841db3ecd47f1ab52ab0daa06d6a9f
tornicke <torniqe@gmail.com>
2022-10-26 19:53:11 +0200

    Importing PortfolioContainer in App.js

--- src/App.js
+++ src/App.js
@@ -1,27 +1,6 @@
-import React, { useState } from "react";
-import About from "./components/About";
-import Footer from "./components/Footer";
-import Header from "./components/Header";
-import "./App.css";
-import Project from "./components/Project";
+import React from "react";
+import PortfolioContainer from "./components/PortfolioContainer";
 
-function App() {
-  let pageComponent;
-  const [page, setPage] = useState("about");
-  console.log("page", page);
-  if (page === "about") {
-    pageComponent = <About />;
-  } else if (page === "work") {
-    pageComponent = <Project />;
-  }
-
-  return (
-    <>
-      <Header setPage={setPage} />
-      {pageComponent}
-      <Footer />
-    </>
-  );
-}
+const App = () => <PortfolioContainer />;
 
 export default App;
