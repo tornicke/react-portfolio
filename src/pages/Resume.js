d16f017cef73e3059bfdddd0944e7db06642efad
tornicke <torniqe@gmail.com>
2022-10-26 19:33:01 +0200

    Added the download link to the resume page

---
+++ src/pages/Resume.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+export default function Resume() {
+  return (
+    <div class="resume">
+      <h1>Resume</h1>
+      <div>
+        Click{" "}
+        <a
+          href="https://drive.google.com/file/d/1ottHee_iLERv4iVOn5zaDrD7rsAj_6TH/view?usp=sharing"
+          download
+        >
+          here
+        </a>{" "}
+        to download my resume.
+      </div>
+    </div>
+  );
+}
